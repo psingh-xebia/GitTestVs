c653dcc0be9d593c13ec16971dc9427f5e27fc57
prabhat <prabhat.prakusi@gmail.com>
2018-03-03 20:31:19 +0530

    Added Method "ADD" in Feature1.cs

--- ConsoleApp1/ConsoleApp1/Feature1.cs
+++ ConsoleApp1/ConsoleApp1/Feature1.cs
@@ -8,5 +8,12 @@ namespace ConsoleApp1
 {
     class Feature1
     {
+        public int Add()
+        {
+            int i = 1;
+            int j = 2;
+            int sum = i + j;
+            return sum;
+        }
     }
 }
